dc8a2ab8085f4c5f876448a5b70dc287eed26856
Quentin <quentin@emmerge.com>
2020-06-28 12:13:33 -0400

    First commit

---
+++ index.js
@@ -0,0 +1,50 @@
+require('dotenv').config();
+const puppeteer = require('puppeteer');
+const fs = require('fs');
+const nodemailer = require('nodemailer');
+
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: process.env.GMAIL_ACCOUNT,
+    pass: process.env.GMAIL_PASSWORD
+  }
+});
+
+(async () => {
+  const config = JSON.parse(fs.readFileSync('./config.json', { encoding: 'utf8' }));
+  const browser = await puppeteer.launch({ headless: false, sloMo: 400 });
+
+  const files = fs.readdirSync('.');
+  const validEntries = config.filter(entry =>
+    !files.some(fileName => fileName.match(entry.name))
+  )
+
+  await Promise.all(validEntries.map(async (entry) => {
+    const page = await browser.newPage();
+    try {
+      await page.goto(entry.page);
+      await page.waitForXPath(entry.selector);
+      await page.close();
+    } catch(error) {
+      const mailOptions = {
+        from: process.env.GMAIL_ACCOUNT,
+        to: process.env.SEND_TO,
+        subject: `Changes happened on the ${entry.name} page!`,
+        text: `Go to ${entry.page}`
+      };
+
+      transporter.sendMail(mailOptions, function(error, info){
+        if (error) {
+          console.log(error);
+        } else {
+          console.log('Email sent: ' + info.response);
+        }
+      });
+
+      await page.screenshot({path: `${entry.name}.png` });
+    }
+  }));
+
+  await browser.close();
+})();
